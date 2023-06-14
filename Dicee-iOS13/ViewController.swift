e76e75e134581df4edeb9c8040dcb7aa67d0da4a
amritanshu_dash <amritanshudash6677@gmail.com>
2023-06-14 22:27:39 +0530

    1st version

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstDiceController: UIImageView!
+    @IBOutlet weak var secondDiceController: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        firstDiceController.alpha = 0.7
+        secondDiceController.alpha = 0.5
     }
 
-
+    @IBAction func diceRollButton(_ sender: UIButton) {
+        
+        let picture_array = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+       
+        firstDiceController.image = picture_array.randomElement() // picture_array[Int.random(in: 0...5)]
+        secondDiceController.image = picture_array.randomElement()
+    }
+    
 }
 
